bd65a4f6568d4864b928e083e96ab15ab36ac73f
Alif Maulana Arifin <alif.maulana@citra.digital>
2024-02-11 14:24:49 +0700

    feat: implement scaffold.

--- lib/main.dart
+++ lib/main.dart
@@ -12,50 +12,25 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text("Flutter is Fun"),
-            backgroundColor: Colors.yellow,
-          ),
-          // Center is like A container but it sis specified for centering things
-          body: Column(
-            children: [
-              Center(
-                child: Container(
-                  child:
-                      const Text("I ❤ Flutter, this is written in a container"),
-                  margin: EdgeInsets.all(100),
-                  color: Colors.blue,
-                  padding: EdgeInsets.all(10),
-                  height: 100,
-                  width: 100,
-                ),
-              ),
-              Container(
-                // Karena pakai kolom, makanya dia kebawah, main axis = ke bawah
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Icon(Icons.menu),
-                    Icon(Icons.abc),
-                    Icon(Icons.ac_unit)
-                  ],
-                ),
-              ),
-              Container(
-                // Karena pakai row, makanya dia samping, main axis = ke samping
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Icon(Icons.menu),
-                    Icon(Icons.abc),
-                    Icon(Icons.ac_unit)
-                  ],
-                ),
-              ),
-            ],
-          )),
+        appBar: AppBar(
+          title: const Text("Flutter is Fun"),
+          backgroundColor: Colors.yellow,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings), label: "Settings"),
+            BottomNavigationBarItem(icon: Icon(Icons.flag), label: "FLag"),
+          ],
+        ),
+        drawer: Drawer(child: Text("Yowasap")),
+        // Center is like A container but it sis specified for centering things
+      ),
     );
   }
 }
