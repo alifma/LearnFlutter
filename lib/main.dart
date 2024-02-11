7e126c200a1ef85bccde622c4bcf4f322a0a1b5a
Alif Maulana Arifin <alif.maulana@citra.digital>
2024-02-11 14:01:57 +0700

    feat: learn about container.

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,18 @@ class MyApp extends StatelessWidget {
           title: const Text("Flutter is Fun"),
           backgroundColor: Colors.yellow,
         ),
+        // Center is like A container but it sis specified for centering things
+        body: Center(
+          // Container is like A Div on HTML, its just wrap a things
+          child: Container(
+            child: const Text("I ❤ Flutter, this is written in a container"),
+            margin: EdgeInsets.all(100),
+            color: Colors.blue,
+            padding: EdgeInsets.all(10),
+            height: 100,
+            width: 100,
+          ),
+        ),
       ),
     );
   }
