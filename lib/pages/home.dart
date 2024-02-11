7ad1753d8db9cc84d70cc30e7d8ad4b06649d425
Alif Maulana Arifin <alif.maulana@citra.digital>
2024-02-11 22:05:42 +0700

    feat: implement popular and finalize project.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,21 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_playground/models/category_models.dart';
 import 'package:flutter_playground/models/diet_models.dart';
+import 'package:flutter_playground/models/popular_models.dart';
 import 'package:flutter_svg/svg.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({Key? key}) : super(key: key);
   List<CategoryModel> categories = [];
   List<DietModel> diets = [];
+  List<PopularDietsModel> popularDiets = [];
 
   void _getInitialInfo() {
     categories = CategoryModel.getCategories();
     diets = DietModel.getDiets();
-  }
-
-  @override
-  void initState() {
-    _getInitialInfo();
+    popularDiets = PopularDietsModel.getPopularDiets();
   }
 
   @override
@@ -24,16 +22,109 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: appBar(),
       backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          _searchField(),
-          const SizedBox(height: 40),
-          _categoriesSection(),
-          const SizedBox(height: 40),
-          _dietSection()
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _searchField(),
+            const SizedBox(height: 40),
+            _categoriesSection(),
+            const SizedBox(height: 40),
+            _dietSection(),
+            const SizedBox(height: 40),
+            _popularSection(),
+            const SizedBox(height: 40),
+          ],
+        ),
       ),
+    );
+  }
+
+  Column _popularSection() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Padding(
+          padding: EdgeInsets.only(left: 20),
+          child: Text(
+            'Keur Rame',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        ListView.separated(
+          itemCount: popularDiets.length,
+          shrinkWrap: true,
+          separatorBuilder: (context, index) => const SizedBox(
+            height: 25,
+          ),
+          padding: const EdgeInsets.only(left: 20, right: 20),
+          itemBuilder: (context, index) {
+            return Container(
+              height: 100,
+              decoration: BoxDecoration(
+                  color: popularDiets[index].boxIsSelected
+                      ? Colors.white
+                      : Colors.transparent,
+                  borderRadius: BorderRadius.circular(16),
+                  boxShadow: popularDiets[index].boxIsSelected
+                      ? [
+                          BoxShadow(
+                              color: const Color(0xff1D1617).withOpacity(0.07),
+                              offset: const Offset(0, 10),
+                              blurRadius: 40,
+                              spreadRadius: 0)
+                        ]
+                      : []),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SvgPicture.asset(
+                    popularDiets[index].iconPath,
+                    width: 65,
+                    height: 65,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        popularDiets[index].name,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black,
+                            fontSize: 16),
+                      ),
+                      Text(
+                        '${popularDiets[index].level} | ${popularDiets[index].duration} | ${popularDiets[index].calorie}',
+                        style: const TextStyle(
+                          color: Color(0xff7B6F72),
+                          fontSize: 13,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: SvgPicture.asset(
+                      'assets/icons/button.svg',
+                      width: 30,
+                      height: 30,
+                    ),
+                  )
+                ],
+              ),
+            );
+          },
+        )
+      ],
     );
   }
 
@@ -46,7 +137,10 @@ class HomePage extends StatelessWidget {
           child: Text(
             'Saran Kanggo Diet',
             style: TextStyle(
-                color: Colors.black, fontSize: 18, fontWeight: FontWeight.w600),
+              color: Colors.black,
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+            ),
           ),
         ),
         const SizedBox(height: 15),
@@ -215,9 +309,7 @@ class HomePage extends StatelessWidget {
             contentPadding: const EdgeInsets.all(15),
             prefixIcon: Padding(
               padding: const EdgeInsets.all(12),
-              child: SvgPicture.asset(
-                "assets/icons/Search.svg",
-              ),
+              child: SvgPicture.asset("assets/icons/Search.svg"),
             ),
             suffixIcon: SizedBox(
               width: 100,
@@ -233,9 +325,7 @@ class HomePage extends StatelessWidget {
                     ),
                     Padding(
                       padding: const EdgeInsets.all(12),
-                      child: SvgPicture.asset(
-                        "assets/icons/Filter.svg",
-                      ),
+                      child: SvgPicture.asset("assets/icons/Filter.svg"),
                     ),
                   ],
                 ),
